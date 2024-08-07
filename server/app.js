abaabb042b0604a488bb0e508ce17e2c9b95a3f7
MTouthang <53576865+MTouthang@users.noreply.github.com>
2024-08-07 22:13:39 +0530

    feat: error handling

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import { config } from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 config();
 const app = express();
@@ -15,4 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
 app.use(morgan("dev"));
+
+app.use(errorMiddleware);
+
 export default app;
---
+++ server/middlewares/ayncHandler.middleware.js
@@ -0,0 +1,14 @@
+function asyncHandler(fn) {
+  return function (req, res, next) {
+    fn(re, res, next).catch(function (err) {
+      next(err);
+    });
+  };
+}
+
+export default asyncHandler;
+
+// arrow function
+// const asyncHandler = (fn) => (req, res, next) => {
+//   fn(req, res, next).catch((err) => next(err));
+// };
